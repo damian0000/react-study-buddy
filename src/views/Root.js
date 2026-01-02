fd1825a1dff8f3352e51fdd28fd597aa357ae3d3
Damian Matoga <damian.matoga@wp.pl>
2026-01-02 18:08:58 +0100

    Add users list component

---
+++ src/views/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import UsersList  from 'components/UsersList/UsersList';
+
+const Root = () => {
+    return (
+        <>
+            <UsersList/>
+        </>
+    );
+};
+
+
+export default Root;
